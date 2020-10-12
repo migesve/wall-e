39ddb172052528b0b1130799e12da91a48302184
Arita Latifi <arita_latifi@hotmail.fr>
2020-10-12 15:50:15 +0200

    nouvelle version

--- Robot/src/Perception.java
+++ Robot/src/Perception.java
@@ -7,28 +7,39 @@ import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
 
 public class Perception {
+
 	private boolean touch;
 	EV3TouchSensor touchSensor;
 	EV3ColorSensor colorSensor;
+	SampleProvider colorProvider;
+	float [] colorSample; //dans case 0 == red, case 1 == green, case 2 == blue
+	float R,G,B;
 
-	public int getCouleur() {
-		Port capteurCouleur = LocalEV3.get().getPort("D");
+	public void getCouleur() { //sans retour mais met les valeur dans R, G, B
+		Port capteurCouleur = LocalEV3.get().getPort("2"); //capteur couleur = le capteur numéro 2
 		colorSensor=new EV3ColorSensor(capteurCouleur);
-		return colorSensor.getColorIDMode();
+		colorProvider =colorSensor.getRGBMode();
+		colorSample = new float [colorProvider.sampleSize()];
+		colorProvider.fetchSample(colorSample, 0);
+		R=colorSample[0];
+		G=colorSample[1];
+		B=colorSample[2];
+
 	}
 
 
 	public boolean getTouche() {
 		Port capteurTouche = LocalEV3.get().getPort("1");
 		touchSensor=new EV3TouchSensor(capteurTouche);
-		
-		if ( touchSensor.getTouchMode == 1)
-			this.b=true;
-		else
-			this.b=false;
-			return b ;
-
-
-
-
+		SampleProvider sampleProvider=touchSensor.getTouchMode();
+		float [] sample=new float [sampleProvider.sampleSize()];
+		if ( sample[0]== 1){
+			this.touch=true;
+			return true;
+		}
+		else {
+			this.touch=false;
+			return touch; 
+		}
 	}
+
