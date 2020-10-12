2c74e41a7b7d767265901d394aa561687efe6c6d
Arita Latifi <arita_latifi@hotmail.fr>
2020-10-12 13:28:05 +0200

    arita

--- Robot/src/Perception.java
+++ Robot/src/Perception.java
@@ -1,7 +1,26 @@
 
 public class Perception {
-	
-	
+	private boolean touch;
+	EV3TouchSensor touchSensor;
+	EV3ColorSensor colorSensor;
 
-}
-	
+	public int getCouleur() {
+		Port capteurCouleur = LocalEV3.get().getPort("D");
+		colorSensor=new EV3ColorSensor(capteurCouleur);
+		return colorSensor.getColorIDMode();
+	}
+
+
+	public boolean getTouche() {
+		Port capteurTouche = LocalEV3.get().getPort(1);
+		touchSensor=new EV3TouchSensor(capteurTouch);
+		if ( touchSensor.getTouchMode == 1)
+			this.b=true;
+		else
+			this.b=false
+			return b ;
+
+
+
+
+	}
