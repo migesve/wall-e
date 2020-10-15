2ac44859c766941914fdea9decba8335f586aaac
Miguelangel Espinal <miguelangelespinal@hotmail.com>
2020-10-15 16:52:04 +0200

    miges

--- Robot/src/Perception.java
+++ Robot/src/Perception.java
@@ -1,26 +1,36 @@
+package src;
+
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
+import lejos.utility.Delay;
 
 //mauvaise version
 public class Perception {
 	private EV3ColorSensor capteurCouleur;
 	private EV3UltrasonicSensor capteurUltra;
 	private EV3TouchSensor capteurTouche;
+	private EV3IRSensor distanceSensor;
+    private static float[] distance;
+    private static SampleProvider average;
 	private float [] colorSample; // tableau des couleurs
 	private SampleProvider colorProvider;
 	private int R,G,B;
 	
 	
 	// ********************** CONSTRUCTEUR ******************************
-	public Perception (Port couleur, Port ultra, Port touche) {
+	public Perception (Port couleur, Port ultra, Port touche, Port IRSensor) {
+    	distanceSensor = new EV3IRSensor(IRSensor);
 		capteurCouleur = new EV3ColorSensor (couleur);
 		capteurUltra = new EV3UltrasonicSensor (ultra);
-		capteurTouche= new EV3TouchSensor (touche);
+		capteurTouche= new EV3TouchSensor (touche); //IRSensor
+    	average = new MeanFilter(distanceSensor.getDistanceMode(), 1);
+    	distance=new float[average.sampleSize()];
 		colorProvider=capteurCouleur.getRGBMode();
 		colorSample = new float [colorProvider.sampleSize()];
 		R=0;
