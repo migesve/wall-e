cabde9f262ab87eebb45df97581f71a61b112896
Arita Latifi <arita_latifi@hotmail.fr>
2020-10-15 16:33:02 +0200

    version 2.0

--- Robot/src/Perception.java
+++ Robot/src/Perception.java
@@ -6,52 +6,90 @@ import lejos.hardware.port.Port;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
 
+//mauvaise version
 public class Perception {
-
-
-	private boolean touch;
-	EV3TouchSensor touchSensor;
-	static EV3ColorSensor colorSensor;
-	static SampleProvider colorProvider;
-	static float [] colorSample; //dans case 0 == red, case 1 == green, case 2 == blue
-	static int R,G,B;
-
-
-	public static void getCouleur() { //sans retour mais met les valeur dans R, G, B
-		Port capteurCouleur = LocalEV3.get().getPort("S2"); //capteur couleur = le capteur numéro 2
-		colorSensor=new EV3ColorSensor(capteurCouleur);
-		colorProvider =colorSensor.getRGBMode();
+	private EV3ColorSensor capteurCouleur;
+	private EV3UltrasonicSensor capteurUltra;
+	private EV3TouchSensor capteurTouche;
+	private float [] colorSample; // tableau des couleurs
+	private SampleProvider colorProvider;
+	private int R,G,B;
+	
+	
+	// ********************** CONSTRUCTEUR ******************************
+	public Perception (Port couleur, Port ultra, Port touche) {
+		capteurCouleur = new EV3ColorSensor (couleur);
+		capteurUltra = new EV3UltrasonicSensor (ultra);
+		capteurTouche= new EV3TouchSensor (touche);
+		colorProvider=capteurCouleur.getRGBMode();
 		colorSample = new float [colorProvider.sampleSize()];
+		R=0;
+		G=0;
+		B=0;	
+		
+	}
+
+
+	
+	// ************************* GETTERS ************************************
+	public EV3ColorSensor getCapteurCouleur() {
+		return capteurCouleur;
+	}
+
+
+	public EV3UltrasonicSensor getCapteurUltra() {
+		return capteurUltra;
+	}
+
+
+	public EV3TouchSensor getCapteurTouche() {
+		return capteurTouche;
+	}
+
+
+	public float[] getColorSample() {
+		return colorSample;
+	}
+
+
+	public int getR() {
+		return R;
+	}
+
+
+	public int getG() {
+		return G;
+	}
+
+
+	public int getB() {
+		return B;
+	}
+	
+	public SampleProvider getColorProvider() {
+		return colorProvider;
+	}
+	
+	// ****************************** METHODES******************************
+	public void getCouleur() {
 		colorProvider.fetchSample(colorSample, 0);
 		R=(int)(255*colorSample[0]);
 		G=(int)(255*colorSample[1]);
 		B=(int)(255*colorSample[2]);
-		System.out.println(R);
-		System.out.println(G);	
-		System.out.println(B);	
-		Delay.msDelay(10000);
-
-	}
-
-
-	public boolean getTouche() {
-		Port capteurTouche = LocalEV3.get().getPort("1");
-		touchSensor=new EV3TouchSensor(capteurTouche);
-		SampleProvider sampleProvider=touchSensor.getTouchMode();
-		float [] sample=new float [sampleProvider.sampleSize()];
-		if ( sample[0]== 1){
-			this.touch=true;
-			return true;
 		}
-		else {
-			this.touch=false;
-			return touch; 
-		}
-	}
 
-	public static void main(String[] args) {
-		getCouleur();
-	}
 
-	}
 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
