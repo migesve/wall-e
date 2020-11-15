f9ec1ff06430866d51f9276daff4a99e3e37e765
GentilBastien <bastien.gattaciecca@gmail.com>
2020-11-15 16:48:20 +0100

    last update Agent

--- Robot/src/Agent.java
+++ Robot/src/Agent.java
@@ -1,6 +1,5 @@
-package src;
-
 import java.awt.Color;
+
 import lejos.hardware.ev3.LocalEV3;
 import lejos.utility.Timer;
 import lejos.utility.TimerListener;
@@ -12,6 +11,7 @@ public class Agent {
 
 	private boolean requestPerpendiculaire;
 	private boolean requestLineFollower;
+	private boolean requestPrendrePalet;
 
 	public Agent() {
 		action = new Actionneur(
@@ -24,6 +24,13 @@ public class Agent {
 				LocalEV3.get().getPort("S3"));
 		timer = new Timer(20,new Boucle());
 		timer.start();
+	}
+	public void prendrePalet() {
+		if (!action.getStatePinces()) {
+			action.ouvrirPinces();
+		}
+		action.avancer(100, true);
+		requestPrendrePalet = true;
 	}
 	//Suit la ligne de couleur c
 	public void lineFollower(Color c) {
@@ -46,17 +53,22 @@ public class Agent {
 			action.update();
 			perception.update();
 			if(requestPerpendiculaire && perception.distance > previousDist) {
+				requestPerpendiculaire = false;
 				action.stop();
 				action.updateDirection(action.getMouvement().getAngleTurned());
-				requestPerpendiculaire = false;
 			}
 			if(requestLineFollower && perception.currentColor != previousColor) {
-				action.stop();
 				requestLineFollower = false;
+				action.stop();
+			}
+			if (requestPrendrePalet && perception.touch) {
+				requestPrendrePalet = false;
+				action.stop();
+				action.fermerPinces();
 			}
 			//les variables "previous" prennent la valeur des variables de perception actuelles
-			//à la fin de la méthode. Ainsi, lorsqu'on appelera cette méthode dans 20s, on commencera
-			//par appeler les méthodes update() pour mettre à jour les nouvelles variables, et on aura
+			//Ã  la fin de la mÃ©thode. Ainsi, lorsqu'on appelera cette mÃ©thode dans 20s, on commencera
+			//par appeler les mÃ©thodes update() pour mettre Ã  jour les nouvelles variables, et on aura
 			//toujours les valeurs des anciennes variables dans les "previous".
 			previousDist = perception.distance;
 			previousColor = perception.currentColor;
