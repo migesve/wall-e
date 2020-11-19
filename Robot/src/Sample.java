31cff9108587ad43a03b0965dd77582ae3c6b1b7
GentilBastien <bastien.gattaciecca@gmail.com>
2020-11-19 23:50:07 +0100

    Sample ZE update

---
+++ Robot/src/Sample.java
@@ -0,0 +1,50 @@
+import lejos.hardware.Button;
+import lejos.robotics.filter.MeanFilter;
+/**
+ * Un Sample peut être traduit par échantillon ou vecteur. Il s'agit des trois quantités de R G B.
+ * Un Sample utilise un MeanFilter pour moyenner le Sample perçu avec N précédents. On définit un
+ * nom pour un sample, c'est important (pour les couleurs principales) d'avoir un nom type String 
+ * pour pouvoir les différencier ou les comparer entre eux facilement ensuite.
+ * @author nous <3
+ */
+public class Sample {
+	private MeanFilter average;
+	private final String colorName;
+	private float[] sample;
+	
+	public Sample(MeanFilter average, String name) {
+		this.average = average;
+		colorName = name;
+		sample = new float[average.sampleSize()];
+	}
+	public Sample(MeanFilter average, String name, float[] sample) {
+		this(average,name);
+		this.sample = sample;
+	}
+	/**
+	 * On utilisera cette méthode lors du calibrage des sample des couleurs principales.
+	 * La méthode waitForPress est bloquante, et nous permettra de déplacer le capteur de
+	 * couleur au dessus de celle qu'on souhaite calibrer.
+	 * Puis ensuite on détecte la couleur.
+	 */
+	public void calibrateColor() {
+		System.out.println("ENTER pour");
+		System.out.println("detecter "+colorName+"..");
+		Button.ENTER.waitForPress();
+		detectColor();
+	}
+	/**
+	 * Détecte une couleur, permet de construire le sample à partir du sample provider.
+	 */
+	public void detectColor() {
+		average.fetchSample(sample, 0);
+	}
+	
+	//Accesseurs publics aux attributs d'instance.
+	public float[] getEchantillon() {
+		return sample;
+	}
+	public String getName() {
+		return colorName;
+	}
+}
